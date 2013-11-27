e326fe1c6c80f0c0b382dd5e808f5cc27457f17f
niku <niku@niku.name>
2013-11-27 11:08:28 +0900

    fix replace-to string
    It needs double back-slash for using replace-match function.
    \ ->
    \\

--- test/quick-toggle-test.el
+++ test/quick-toggle-test.el
@@ -4,8 +4,8 @@
         ; set up part
         (let ((map #s(hash-table test equal
                                  data(
-                                      "spec/lib/\\(.+\\)_spec\.rb" "lib/\1.rb"
-                                      "lib/\\(.+\\)\.rb" "spec/lib/\1_spec.rb"
+                                      "spec/lib/\\(.+\\)_spec\.rb" "lib/\\1.rb"
+                                      "lib/\\(.+\\)\.rb" "spec/lib/\\1_spec.rb"
                                       ))))
           ; test part
           (funcall body))
@@ -19,14 +19,14 @@
    (lambda ()
      (should (equal
               (quick-toggle-find-matcher map "/home/niku/projects/foo/lib/foo.rb")
-              (cons "lib/\\(.+\\)\.rb" "spec/lib/\1_spec.rb"))))))
+              (cons "lib/\\(.+\\)\.rb" "spec/lib/\\1_spec.rb"))))))
 
 (ert-deftest quick-toggle-find-matcher:when-matched-second ()
   (my-fixture
    (lambda ()
      (should (equal
               (quick-toggle-find-matcher map "/home/niku/projects/foo/spec/lib/foo_spec.rb")
-              (cons "spec/lib/\\(.+\\)\_spec\.rb" "lib/\1.rb"))))))
+              (cons "spec/lib/\\(.+\\)\_spec\.rb" "lib/\\1.rb"))))))
 
 (ert-deftest quick-toggle-find-matcher:when-unmatched ()
   (my-fixture
